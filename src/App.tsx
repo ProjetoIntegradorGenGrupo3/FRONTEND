942f92f02e95c7e35124d50576761e7a50933112
Carolimaa <carolniver12@gmail.com>
2024-01-19 16:31:23 -0300

    Implementação da Pagina Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Footer from "./Components/Footer/footer"
 import Navbar from "./Components/NavBar/navBar"
+import Home from "./pages/home/home"
 import Login from "./pages/login/Login"
 import { BrowserRouter, Route, Routes} from 'react-router-dom'
 
@@ -14,7 +15,7 @@ const App = () => {
        <Routes>
           <Route path='/' element={<Login />}  /> 
           <Route path='/login' element={<Login />}  /> 
-          {/* <Route path='/home' element={<Home />}  />  */}
+          { <Route path='/home' element={<Home />}  />  }
           {/* <Route path='/sobre' element={<Sobre />}  />  */}
        </Routes>
        </div>
