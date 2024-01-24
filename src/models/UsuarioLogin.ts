d6006c47516c774f855e73a9840661c3a0de5be8
Matheus <mattzeppelin.mj@gmail.com>
2024-01-24 14:09:40 -0300

    Manutenção da pagina Cadastro

--- src/models/UsuarioLogin.ts
+++ src/models/UsuarioLogin.ts
@@ -2,8 +2,8 @@ export default interface UsuarioLogin {
     id: number;
     nome: string;
     email: string;
-    foto: string;
     senha: string;
+    foto: string;
     bio: string;
     contato: string;
     data_nascimento: string;
--- src/service/Service.ts
+++ src/service/Service.ts
@@ -6,10 +6,33 @@ const api = axios.create({
 
 export const cadastrarUsuario = async(url: string, dados: Object, setDados: Function) => {
   const resposta = await api.post(url, dados)
-  setDados(resposta.data)
+  setDados(resposta.data) //recebe o retorno do back e salvar os dados do usuario
 }
 
 export const login = async(url: string, dados: Object, setDados: Function) => {
-    const resposta = await api.post(url, dados)
-    setDados(resposta.data)
-  }
+  const resposta = await api.post(url, dados)
+  setDados(resposta.data)
+}
+
+// Service que será usada para BUSCAR informações ao Back-End
+export const buscar = async (url: string, setDados: Function, header: Object) => {
+  const resposta = await api.get(url, header) // espera a requisição do tipo GET, enviada pela rota informada dentro do parametro URL, seja finalizada. Também enviamos o Token através do parametro HEADER
+  setDados(resposta.data)                     // salva as info retornadas em uma variavel de estado
+}
+
+// Service que será usada para CADASTRAR informações no Back-End
+export const cadastrar = async (url: string, dados: Object, setDados: Function, header: Object) => {
+  const resposta = await api.post(url, dados, header) // espera a requisição do tipo POST, enviada pela rota informada dentro do parametro URL, seja finalizada. Também enviamos o Token através do parametro HEADER
+  setDados(resposta.data)                             // salva as info retornadas em uma variavel de estado
+}
+
+// Service que será usada para ATUALIZAR informações do Back-End
+export const atualizar = async (url: string, dados: Object, setDados: Function, header: Object) => {
+  const resposta = await api.put(url, dados, header)  // espera a requisição do tipo POST, enviada pela rota informada dentro do parametro URL, seja finalizada. Também enviamos o Token através do parametro HEADER
+  setDados(resposta.data)                             // salva as info retornadas em uma variavel de estado
+}
+
+// Service que será usada para APAGAR informações do Back-End
+export const deletar = async (url: string, header: Object) => {
+  await api.delete(url, header)   // espera a requisição do tipo DELETE, enviada pela rota informada dentro do parametro URL, seja finalizada. Também enviamos o Token através do parametro HEADER
+}
