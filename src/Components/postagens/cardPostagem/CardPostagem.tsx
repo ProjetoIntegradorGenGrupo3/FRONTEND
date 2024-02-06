f481a3dff2a4b907098ae7fa6021d6c37953f1ac
Vinicius Lima <vinicius.slima99@icloud.com>
2024-02-06 15:39:42 -0300

    atualizando o sobre.tsx

--- src/Components/postagens/cardPostagem/CardPostagem.tsx
+++ src/Components/postagens/cardPostagem/CardPostagem.tsx
@@ -22,7 +22,7 @@ function CardPostagem({ post }: CardPostagemProps) {
 
     return (
    
-            <div className='border-claro shadow-xl border flex-col rounded overflow-hidden w-1/2 text-claro '>
+        <div className='border-claro shadow-xl border flex-col rounded overflow-hidden w-1/2 mx-auto text-claro card'>
                 <div className='bg-white'>
                     <div className="flex w-full bg-azmedio py-1 px-4 items-center gap-4">
                         <img src={post.usuario?.foto} className='h-12 rounded-full' alt="" />   
@@ -32,7 +32,7 @@ function CardPostagem({ post }: CardPostagemProps) {
                         <h4 className='text-lg font-semibold uppercase'>{post.localidade}</h4> 
                         <p>{post.conteudo}</p>
                         <p>Tema: {post.tipo?.descricao}</p>
-                        <img src={post.foto} className='h-80' alt="foto do post" /> 
+                        <img src={post.foto} className='h-80 w-full' alt="foto do post" /> 
                         <p>Data: {dataLocal}</p>
                     </div>
                 </div>
