2e17e69d349b9eb0daf9b0cf6a02d80d94d00e6f
Vinicius Lima <vinicius.slima99@icloud.com>
2024-02-06 18:02:30 -0300

    Atualização da página sobre.tsx

--- src/pages/sobre/sobre.tsx
+++ src/pages/sobre/sobre.tsx
@@ -1,45 +1,145 @@
-import Vini from '../../assets/vini-avatar.png'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Carol from '../../assets/carol-avatar.webp';
+import Gabriel from '../../assets/gabriel.png';
+import Matheus from '../../assets/matheus-avatar.webp';
+import Pedro from '../../assets/pedro-avatar.webp';
+import Talia from '../../assets/talia-avatar.webp';
+import Vini from '../../assets/vini-avatar.png';
+
 
 function Sobre() {
-    return (
-        <>
-            <div className="max-w-2xl mx-4 sm:max-w-sm md:max-w-sm lg:max-w-sm xl:max-w-sm sm:mx-auto md:mx-auto lg:mx-auto xl:mx-auto mt-16 bg-white shadow-xl rounded-lg text-gray-900">
-  <div className="rounded-t-lg h-32 overflow-hidden">
-  </div>
-  <div className="mx-auto relative -mt-16 border-4 border-white overflow-hidden">
-    <img className="object-cover object-center h-64 mx-auto" src={Vini} alt='Woman looking front'/>
-  </div>
-  <div className="text-center mt-2">
-    <h2 className="font-semibold">Vinicius Lima</h2>
-    <p className="text-gray-500">Desenvolvedor Fullstack</p>
-  </div>
-  <ul className="py-4 mt-2 text-gray-700 flex items-center justify-around">
-    <li className="flex flex-col items-center justify-around">
-      <svg className="w-4 fill-current text-blue-900" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-        <path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z"/>
-      </svg>
-      <div>2k</div>
-    </li>
-    <li className="flex flex-col items-center justify-between">
-      <svg className="w-4 fill-current text-blue-900" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-        <path d="M7 8a4 4 0 1 1 0-8 4 4 0 0 1 0 8zm0 1c2.15 0 4.2.4 6.1 1.09L12 16h-1.25L10 20H4l-.75-4H2L.9 10.09A17.93 17.93 0 0 1 7 9zm8.31.17c1.32.18 2.59.48 3.8.92L18 16h-1.25L16 20h-3.96l.37-2h1.25l1.65-8.83zM13 0a4 4 0 1 1-1.33 7.76 5.96 5.96 0 0 0 0-7.52C12.1.1 12.53 0 13 0z"/>
-      </svg> 
-      <div>10k</div>
-    </li>
-    <li className="flex flex-col items-center justify-around">
-      <svg className="w-4 fill-current text-blue-900" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-        <path d="M9 12H1v6a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-6h-8v2H9v-2zm0-1H0V5c0-1.1.9-2 2-2h4V2a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v1h4a2 2 0 0 1 2 2v6h-9V9H9v2zm3-8V2H8v1h4z"/>
-      </svg>
-      <div>15</div>
-    </li>
-  </ul>
-  <div className="p-4 border-t mx-8 mt-2">
-    <button className="w-1/2 block mx-auto rounded-full bg-gray-900 hover:shadow-lg font-semibold text-white px-6 py-2">Follow</button>
-  </div>
-</div>
+  return (
+    <>
+      <div>
+        <h1 className='flex justify-center text-4xl'>Equipe de Desenvolvimento</h1>
+        <p className='flex justify-center text-2xl py-2'>
+          Conheça o time por trás do projeto!
+        </p>
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-4">
+        <div className="max-w-2xl mx-4 sm:max-w-sm md:max-w-sm lg:max-w-sm xl:max-w-sm sm:mx-auto md:mx-auto lg:mx-auto xl:mx-auto mt-16 bg-white shadow-xl rounded-lg text-gray-900">
+          <div className="rounded-t-lg h-32 overflow-hidden">
+          </div>
+          <div className="mx-auto relative -mt-16 border-4 border-white overflow-hidden">
+            <img className="object-cover object-center h-64 mx-auto" src={Carol} alt='Avatar da criadora Caroline' />
+          </div>
+          <div className="text-center mt-2 border-t py-2">
+            <h2 className="font-semibold">Caroline Lima</h2>
+            <p className="text-gray-500">Desenvolvedora Fullstack</p>
+          </div>
+          <div className="flex py-2 px-14 justify-evenly space-x-4">
+            <Link to="https://github.com/Carolimaa" target="_blank" className="link-github">
+              <button className="w-full block mx-auto rounded-full bg-gray-900 hover:shadow-lg items-center font-semibold text-white px-6 py-2">GitHub</button>
+            </Link>
+            <Link to="https://www.linkedin.com/in/carolinelimaa/" target="_blank" className="link-github">
+              <button className="w-full block mx-auto rounded-full bg-gray-900 hover:shadow-lg font-semibold text-white px-6 py-2">Linkedin</button>
+            </Link>
+          </div>
+        </div>
 
-        </>
-    );
+        <div className="max-w-2xl mx-4 sm:max-w-sm md:max-w-sm lg:max-w-sm xl:max-w-sm sm:mx-auto md:mx-auto lg:mx-auto xl:mx-auto mt-16 bg-white shadow-xl rounded-lg text-gray-900">
+          <div className="rounded-t-lg h-32 overflow-hidden">
+          </div>
+          <div className="relative justify-stretch -mt-16 border-4 border-white overflow-hidden">
+            <img className="object-cover object-center h-64 mx-auto" src={Gabriel} alt='Avatar do criador Gabriel' />
+          </div>
+          <div className="text-center mt-2 border-t py-2">
+            <h2 className="font-semibold">Gabriel Iwakura</h2>
+            <p className="text-gray-500">Desenvolvedor Fullstack</p>
+          </div>
+          <div className="flex py-2 px-14 justify-evenly space-x-4">
+            <Link to="https://github.com/Gakuraa" target="_blank" className="link-github">
+              <button className="w-full block mx-auto rounded-full bg-gray-900 hover:shadow-lg items-center font-semibold text-white px-6 py-2">GitHub</button>
+            </Link>
+            <Link to="https://www.linkedin.com/in/gabrieliwakura/" target="_blank" className="link-github">
+              <button className="w-full block mx-auto rounded-full bg-gray-900 hover:shadow-lg font-semibold text-white px-6 py-2">Linkedin</button>
+            </Link>
+          </div>
+        </div >
+
+        <div className="max-w-2xl mx-4 sm:max-w-sm md:max-w-sm lg:max-w-sm xl:max-w-sm sm:mx-auto md:mx-auto lg:mx-auto xl:mx-auto mt-16 bg-white shadow-xl rounded-lg text-gray-900">
+          <div className="rounded-t-lg h-32 overflow-hidden">
+          </div>
+          <div className="mx-auto relative -mt-16 border-4 border-white overflow-hidden">
+            <img className="object-cover object-center h-64 mx-auto" src={Matheus} alt='Avatar do criador Vini' />
+          </div>
+          <div className="text-center mt-2 border-t py-2">
+            <h2 className="font-semibold">Matheus Julio</h2>
+            <p className="text-gray-500">Desenvolvedor Fullstack</p>
+          </div>
+          <div className="flex py-2 px-14 justify-evenly space-x-4">
+            <Link to="https://github.com/MatheusJulioSantana" target="_blank" className="link-github">
+              <button className="w-full block mx-auto rounded-full bg-gray-900 hover:shadow-lg items-center font-semibold text-white px-6 py-2">GitHub</button>
+            </Link>
+            <Link to="https://www.linkedin.com/in/matheusjuliosantana/" target="_blank" className="link-github">
+              <button className="w-full block mx-auto rounded-full bg-gray-900 hover:shadow-lg font-semibold text-white px-6 py-2">Linkedin</button>
+            </Link>
+          </div>
+        </div >
+
+        <div className="max-w-2xl mx-4 sm:max-w-sm md:max-w-sm lg:max-w-sm xl:max-w-sm sm:mx-auto md:mx-auto lg:mx-auto xl:mx-auto mt-16 bg-white shadow-xl rounded-lg text-gray-900">
+          <div className="rounded-t-lg h-32 overflow-hidden">
+          </div>
+          <div className="mx-auto relative -mt-16 border-4 border-white overflow-hidden">
+            <img className="object-cover object-center h-64 mx-auto" src={Pedro} alt='Avatar do criador Vini' />
+          </div>
+          <div className="text-center mt-2 border-t py-2">
+            <h2 className="font-semibold">Pedro Viana</h2>
+            <p className="text-gray-500">Desenvolvedor Fullstack</p>
+          </div>
+          <div className="flex py-2 px-14 justify-evenly space-x-4">
+            <Link to="https://github.com/Pedro-Vhiana" target="_blank" className="link-github">
+              <button className="w-full block mx-auto rounded-full bg-gray-900 hover:shadow-lg items-center font-semibold text-white px-6 py-2">GitHub</button>
+            </Link>
+            <Link to="https://www.linkedin.com/in/vhiana-pedro/" target="_blank" className="link-github">
+              <button className="w-full block mx-auto rounded-full bg-gray-900 hover:shadow-lg font-semibold text-white px-6 py-2">Linkedin</button>
+            </Link>
+          </div>
+        </div >
+
+        <div className="max-w-2xl mx-4 sm:max-w-sm md:max-w-sm lg:max-w-sm xl:max-w-sm sm:mx-auto md:mx-auto lg:mx-auto xl:mx-auto mt-16 bg-white shadow-xl rounded-lg text-gray-900">
+          <div className="rounded-t-lg h-32 overflow-hidden">
+          </div>
+          <div className="mx-auto relative -mt-16 border-4 border-white overflow-hidden">
+            <img className="object-cover object-center h-64 mx-auto" src={Talia} alt='Avatar da desenvolvedora Talia' />
+          </div>
+          <div className="text-center mt-2 border-t py-2">
+            <h2 className="font-semibold">Talia Moura</h2>
+            <p className="text-gray-500">Desenvolvedora Fullstack</p>
+          </div>
+          <div className="flex py-2 px-14 justify-evenly space-x-4">
+            <Link to="https://github.com/TahMoura" target="_blank" className="link-github">
+              <button className="w-full block mx-auto rounded-full bg-gray-900 hover:shadow-lg items-center font-semibold text-white px-6 py-2">GitHub</button>
+            </Link>
+            <Link to="https://www.linkedin.com/in/talia-moura/" target="_blank" className="link-github">
+              <button className="w-full block mx-auto rounded-full bg-gray-900 hover:shadow-lg font-semibold text-white px-6 py-2">Linkedin</button>
+            </Link>
+          </div>
+        </div >
+
+        <div className="max-w-2xl mx-4 sm:max-w-sm md:max-w-sm lg:max-w-sm xl:max-w-sm sm:mx-auto md:mx-auto lg:mx-auto xl:mx-auto mt-16 bg-white shadow-xl rounded-lg text-gray-900">
+          <div className="rounded-t-lg h-32 overflow-hidden">
+          </div>
+          <div className="mx-auto relative -mt-16 border-4 border-white overflow-hidden">
+            <img className="object-cover object-center h-64 mx-auto" src={Vini} alt='Avatar do criador Vini' />
+          </div>
+          <div className="text-center mt-2 border-t py-2">
+            <h2 className="font-semibold">Vinicius Lima</h2>
+            <p className="text-gray-500">Desenvolvedor Fullstack</p>
+          </div>
+          <div className="flex py-2 px-14 justify-evenly space-x-4">
+            <Link to="https://github.com/vinysl" target="_blank" className="link-github">
+              <button className="w-full block mx-auto rounded-full bg-gray-900 hover:shadow-lg items-center font-semibold text-white px-6 py-2">GitHub</button>
+            </Link>
+            <Link to="https://www.linkedin.com/in/vinicius-lima-7b3b23235/" target="_blank" className="link-github">
+              <button className="w-full block mx-auto rounded-full bg-gray-900 hover:shadow-lg font-semibold text-white px-6 py-2">Linkedin</button>
+            </Link>
+          </div>
+        </div >
+      </div>
+    </>
+  );
 }
 
-export default Sobre
+export default Sobre;
