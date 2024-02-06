2f4a247a8b855db2566a84ad7882b9ebd1910311
Gakuraa <gabrieliwakura924@gmail.com>
2024-02-06 15:07:16 -0300

    Card no Sobre

--- src/pages/sobre/sobre.tsx
+++ src/pages/sobre/sobre.tsx
@@ -1,24 +1,26 @@
-import homeLogo from '../../assets/Sobre.svg'
+import gabriel from '../../assets/gabriel.png'
+
+import './sobre.css';
 
 function Sobre() {
     return (
         <>
-            <div className="flex justify-center h-screen">
-                <div className='container grid grid-cols-2 text-white'>
-                    <div className="flex flex-col gap-4 items-center justify-center py-4">
-                        <h2 className='text-5xl font-bold'>Saiba um pouco mais sobre nós!</h2>
-                        <p className='text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, tempora! Saepe porro et libero molestiae consequuntur quod, ad necessitatibus corrupti cumque id! Eos, nesciunt rerum! Qui ducimus voluptatibus quae perspiciatis.</p>
+            <div className="max-auto max-w-2xl mx-4 sm:max-w-sm md:max-w-sm lg:max-w-sm xl:max-w-sm sm:mx-auto md:mx-auto lg:mx-auto xl:mx-auto mt-10 bg-white shadow-xl rounded-lg text-gray-900">
+                <div className="rounded-t-lg h-32 overflow-hidden">
+                </div>
+                <div className="mx-auto w-32 h-32 relative -mt-16 overflow-hidden mb-10">
+                    <img className="object-center h-32" src={gabriel} alt='Gabriel'/>
+                </div>
+                <div className="text-center mt-2">
+                    <h2 className="font-semibold p-2">Gabriel Iwakura</h2>
+                    <p className="text-escuro">Freelance Web Designer</p>
+                </div>
+                <div>
+                    <h3 className='text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto quod quam quidem ratione fugiat cumque nobis, quibusdam dolores vitae illo similique minima excepturi eveniet consectetur atque quia. Cum, molestiae impedit?</h3>
+                </div>
 
-                        <div className="flex justify-around gap-4">
-
-                            <button className='rounded bg-white text-black py-2 px-4'>Github</button>
-                        </div>
-                    </div>
-
-                    <div className="flex justify-center ">
-                        <img src={homeLogo} alt="" className='w-2/3' />
-
-                    </div>
+                <div className="p-4 border-t mx-8 mt-2 p-2">
+                    <button className="w-1/2 block mx-auto rounded-full bg-escuro hover:shadow-lg font-semibold text-white px-6 py-2">GitHub</button>
                 </div>
             </div>
 
